cd028985b0badeacbc7c35a6c98cec7d9d2c7135
ghaseminya <ghaseminya@gmail.com>
2020-02-09 12:29:48 +0330

    initial commit

---
+++ adhan-swing/src/adhan/swing/dataset.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adhan.swing;
+
+/**
+ *
+ * @author mnm
+ */
+public class dataset {
+    public String[][] countries=new String[][]{{"ایران","1"},{"عراق","2"}};
+    public String[][] provines=new String[][]{{"تهران","1","1"},{"اصفهان","1","2"},{"فارس","1","3"},{"قم","1","4"},{"مشهد","1","5"}};
+    public String[][] city=new String[][]{{"قم","4"},{"",""}};
+    public String[] adhan_file=new String[]{""};
+}
